27a8676328833038fcd1c69fddf0323a40da2c6b
Josh <joshscina@gmail.com>
2024-02-29 18:58:19 -0500

    Update main.rs

--- day_eight/src/main.rs
+++ day_eight/src/main.rs
@@ -1,5 +1,7 @@
+use std::thread;
 use std::{collections::HashMap, fs};
 
+#[derive(Debug, Clone)]
 enum Instruction {
     Right,
     Left,
@@ -18,10 +20,57 @@ impl Node {
     }
 }
 
+fn part_one(
+    current_node: String,
+    map: HashMap<String, (String, String)>,
+    instructions: Vec<Instruction>,
+) -> u32 {
+    let mut count = 0;
+    let mut current_node = current_node;
+
+    for instruction in instructions.iter().cycle() {
+        if current_node.ends_with('Z') {
+            return count;
+        }
+        count += 1;
+        let (left, right) = map.get(current_node.as_str()).unwrap();
+        match instruction {
+            Instruction::Right => current_node = right.to_string(),
+            Instruction::Left => current_node = left.to_string(),
+        }
+    }
+    count
+}
+
+fn part_two(map: HashMap<String, (String, String)>, instructions: Vec<Instruction>) -> u32 {
+    // Start at all the values that end in A, and end at all the values that end in Z.
+    let starting_nodes: Vec<String> = map
+        .keys()
+        .filter(|x| x.ends_with('A'))
+        .map(|x| x.to_string())
+        .collect();
+    // Thread this using the part_one function.
+    let mut threads = vec![];
+    for node in starting_nodes {
+        let map_clone = map.clone();
+        let instructions_clone = instructions.clone();
+        threads.push(thread::spawn(move || {
+            part_one(node, map_clone, instructions_clone)
+        }));
+    }
+    // Sum the results.
+    let mut sum = 0;
+    for thread in threads {
+        sum += thread.join().unwrap();
+    }
+
+    sum
+}
+
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    let mut map: HashMap<&str, (&str, &str)> = HashMap::new();
-    let split_contents = contents.split_once("\n").unwrap();
+    let mut map: HashMap<String, (String, String)> = HashMap::new();
+    let split_contents = contents.split_once('\n').unwrap();
     let instructions: Vec<Instruction> = split_contents
         .0
         .chars()
@@ -44,30 +93,21 @@ fn main() {
             let split2 = split.1.split_once(", ").unwrap();
             return Node::new(
                 split.0.to_string(),
-                split2.0.trim_start_matches("(").to_string(),
-                split2.1.trim_end_matches(")").to_string(),
+                split2.0.trim_start_matches('(').to_string(),
+                split2.1.trim_end_matches(')').to_string(),
             );
         })
         .collect::<Vec<Node>>();
 
     for node in node_network.iter() {
-        map.insert(&node.name, (&node.left, &node.right));
+        map.insert(
+            node.name.to_string(),
+            (node.left.to_string(), node.right.to_string()),
+        );
     }
 
-    let mut current_node = String::from("AAA");
-
-    let mut count = 0;
-
-    for instruction in instructions.iter().cycle() {
-        if current_node == "ZZZ" {
-            println!("Part 1: {}", count);
-            break;
-        }
-        count += 1;
-        let (left, right) = map.get(current_node.as_str()).unwrap();
-        match instruction {
-            Instruction::Right => current_node = right.to_string(),
-            Instruction::Left => current_node = left.to_string(),
-        }
-    }
+    let res1 = part_one("AAA".to_string(), map.clone(), instructions.clone());
+    let res2 = part_two(map, instructions);
+    println!("Part one: {}", res1);
+    println!("Part two: {}", res2);
 }
