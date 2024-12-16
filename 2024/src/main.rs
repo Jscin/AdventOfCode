72b0c23c9068c75892723b676559c150a0e58d56
Josh <joshscina@gmail.com>
2024-12-15 21:35:44 -0500

    add simple cli

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,6 +1,25 @@
 extern crate aoc;
-use aoc::day1;
+use aoc::*;
+use clap::{Arg, Command};
 
 fn main() {
-    day1::run();
+    let matches = Command::new("AOC Runner")
+        .version("1.0")
+        .author("Joshua Scina<joshscina@gmail.com>")
+        .about("Runs Advent of Code solutions")
+        .arg(
+            Arg::new("day")
+                .short('d')
+                .long("day")
+                .value_name("DAY")
+                .help("Specifies which day's solution to run")
+                .required(true),
+        )
+        .get_matches();
+
+    match matches.get_one::<String>("day").map(|s| s.as_str()) {
+        Some("1") => day1::run(),
+        Some("2") => day2::run(),
+        _ => eprintln!("Invalid day specified. Please choose 1 or 2."),
+    }
 }
